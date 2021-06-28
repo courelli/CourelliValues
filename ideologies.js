af885a8629d3f55a53906c6dc9f56f1d74657671
courelli <55363714+courelli@users.noreply.github.com>
2021-06-28 09:08:00 -0500

    Update ideologies.js
    added leftist factions

--- ideologies.js
+++ ideologies.js
@@ -1,15 +1,42 @@
 ideologies = [
     {
-        "name": "World Workers Party", // "Marxism"
+        "name": "World Workers Party (Mainstream)", // "Marxism"
         "stats": {
             "econ": 100,
             "dipl": 70,
-            "govt": 70,
+            "govt": 40,
             "scty": 70
         }
     },
     {
-        "name": "United Left Party", // "Democratic Socialism"
+        "name": "World Workers Party (Anarchist)", // "Anarcho-Communism"
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 90
+        }
+    },
+    {
+        "name": "World Workers Party (Vanguardist)", // "Leninism"
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "World Workers Party (Internationalist)", // "Trotskyism"
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "United Left Party (Mainstream)", // "Democratic Socialism"
         "stats": {
             "econ": 80,
             "dipl": 50,
@@ -18,7 +45,34 @@ ideologies = [
         }
     },
     {
-        "name": "Social Democratic Party", // "Social Democracy"
+        "name": "United Left Party (Revolutionary)", // "Revolutionary Socialism"
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "United Left Party (Libertarian)", // "Libertarian Socialism"
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {
+        "name": "United Left Party (Populist)", // "Left-Wing Populism"
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Social Democratic Party (Mainstream)", // "Social Democracy"
         "stats": {
             "econ": 60,
             "dipl": 70,
@@ -27,7 +81,16 @@ ideologies = [
         }
     },
     {
-        "name": "Liberal Party", // "Liberalism"
+        "name": "Social Democratic Party (Progressive)", // "Progressivism"
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Liberal Party (Mainstream)", // "Liberalism"
         "stats": {
             "econ": 50,
             "dipl": 60,
@@ -36,12 +99,30 @@ ideologies = [
         }
     },
     {
-        "name": "Green Party", // "Technocracy"
+        "name": "Liberal Party (Social Liberal)", // "Social Liberalism"
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Green Party (Mainstream)", // "Technocracy"
         "stats": {
             "econ": 60,
             "dipl": 60,
             "govt": 80,
             "scty": 70
+        }
+    },
+    {
+        "name": "Green Party (Eco-Socialist)", // "Anarcho-Mutualist"
+        "stats": {
+            "econ": 90,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 80
         }
     },
     {
