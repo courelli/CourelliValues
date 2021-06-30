366e68b21e64283b8e861929f8e81119310e41f8
courelli <55363714+courelli@users.noreply.github.com>
2021-06-30 07:52:05 -0500

    Update ideologies.js
    filled out party values

--- ideologies.js
+++ ideologies.js
@@ -2,157 +2,157 @@ ideologies = [
     {
         "name": "World Workers Party", // "Marxism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 90,
+            "govt": 75,
+            "cent": 85,
+            "relg": 5,
+            "socl": 95,
+            "natl": 65,
+            "milt": 25,
+            "ecol": 75
         }
     },
     {
         "name": "United Left Party", // "Democratic Socialism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 80,
+            "govt": 85,
+            "cent": 50,
+            "relg": 25,
+            "socl": 85,
+            "natl": 50,
+            "milt": 45,
+            "ecol": 75
         }
     },
     {
         "name": "Social Democratic Party", // "Social Democracy"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 70,
+            "govt": 95,
+            "cent": 60,
+            "relg": 50,
+            "socl": 65,
+            "natl": 50,
+            "milt": 65,
+            "ecol": 65
         }
     },
     {
         "name": "Liberal Party", // "Liberalism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 60,
+            "govt": 75,
+            "cent": 75,
+            "relg": 50,
+            "socl": 65,
+            "natl": 75,
+            "milt": 85,
+            "ecol": 50
         }
     },
     {
         "name": "Green Party", // "Technocracy"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 75,
+            "govt": 65,
+            "cent": 90,
+            "relg": 10,
+            "socl": 85,
+            "natl": 85,
+            "milt": 0,
+            "ecol": 100
         }
     },
     {
         "name": "Unity Party", // "Centrism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 40,
+            "govt": 75,
+            "cent": 50,
+            "relg": 50,
+            "socl": 40,
+            "natl": 50,
+            "milt": 10,
+            "ecol": 25
         }
     },
     {
         "name": "One Courelli Party", // "Neo-Liberalism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 25,
+            "govt": 50,
+            "cent": 40,
+            "relg": 50,
+            "socl": 40,
+            "natl": 65,
+            "milt": 70,
+            "ecol": 25
         }
     },
     {
         "name": "Liberty Party", // "Ultra-Capitalism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 0,
+            "govt": 95,
+            "cent": 0,
+            "relg": 45,
+            "socl": 35,
+            "natl": 80,
+            "milt": 50,
+            "ecol": 0
         }
     },
     {
         "name": "Conservative Party", // "Conservatism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 10,
+            "govt": 40,
+            "cent": 20,
+            "relg": 30,
+            "socl": 10,
+            "natl": 25,
+            "milt": 85,
+            "ecol": 20
         }
     },
     {
         "name": "National Front", // "Capitalist Fascism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 0,
+            "govt": 15,
+            "cent": 100,
+            "relg": 75,
+            "socl": 0,
+            "natl": 0,
+            "milt": 100,
+            "ecol": 10
         }
     },
     {
         "name": "Dalinian Alliance", // "Fundamentalism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 5,
+            "govt": 25,
+            "cent": 80,
+            "relg": 0,
+            "socl": 15,
+            "natl": 60,
+            "milt": 50,
+            "ecol": 50
         }
     },
     {
         "name": "Afafanua National Party", // "Right-Wing Populism"
         "stats": {
-            "econ": ,
-            "govt": ,
-            "cent": ,
-            "relg": ,
-            "socl": ,
-            "natl": ,
-            "milt": ,
-            "ecol": 
+            "econ": 35,
+            "govt": 85,
+            "cent": 30,
+            "relg": 50,
+            "socl": 50,
+            "natl": 15,
+            "milt": 10,
+            "ecol": 50
         }
     },
 ];
